068a79c116b066bb54fb7e107464e19022cff434
jung <eugenejung04@gmail.com>
2024-04-26 15:26:20 -0400

    signup typo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Signup from './pages/signup';
+import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login'
 
